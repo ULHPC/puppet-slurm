f60a741c3d54880b7b05196d3ab495c1f2dc48ac
Sebastien Varrette <Sebastien.Varrette@uni.lu>
2017-08-20 19:18:03 +0200

    Update & review Vagrant infra
    Signed-off-by: Sebastien Varrette <Sebastien.Varrette@uni.lu>

--- .vagrant_init.rb
+++ .vagrant_init.rb
@@ -1,10 +1,10 @@
-#!/usr/bin/env ruby 
+#!/usr/bin/env ruby
 ##########################################################################
 # vagrant_init.rb
 # @author Sebastien Varrette <Sebastien.Varrette@uni.lu>
-# Time-stamp: <Tue 2015-06-02 22:43 svarrette>
+# Time-stamp: <Sun 2017-08-20 19:16 svarrette>
 #
-# @description 
+# @description
 #
 # Copyright (c) 2014 Sebastien Varrette <Sebastien.Varrette@uni.lu>
 # .             http://varrette.gforge.uni.lu
@@ -19,49 +19,50 @@ include FalkorLib::Common
 # Load metadata
 basedir   = File.directory?('/vagrant') ? '/vagrant' : Dir.pwd
 jsonfile  = File.join( basedir, 'metadata.json')
+puppetdir = '/etc/puppetlabs'
 
 error "Unable to find the metadata.json" unless File.exists?(jsonfile)
 
 metadata   = JSON.parse( IO.read( jsonfile ) )
-name = metadata["name"].gsub(/^[^\/-]+[\/-]/,'') 
+name = metadata["name"].gsub(/^[^\/-]+[\/-]/,'')
 modulepath=`puppet config print modulepath`.chomp
 moduledir=modulepath.split(':').first
 
 
-run %{ cd /etc/puppet && librarian-puppet clean && rm Puppetfile* }
-run %{ ln -s /vagrant/metadata.json /etc/puppet/ }
-run %{ cd /etc/puppet && librarian-puppet install --verbose }
+run %{ cd #{moduledir}/.. && librarian-puppet clean && rm Puppetfile* }
+run %{ ln -s /vagrant/metadata.json #{moduledir}/../ }
+run %{ cd #{moduledir}/.. && librarian-puppet install --verbose }
 
 
 # metadata["dependencies"].each do |dep|
 # 	lib = dep["name"]
 #     shortname = lib.gsub(/^.*[\/-]/,'')
 #     action = File.directory?("#{moduledir}/#{shortname}") ? 'upgrade --force' : 'install'
-# 	run %{ puppet module #{action} #{lib} } 
+# 	run %{ puppet module #{action} #{lib} }
 # end
 
 puts "Module path: #{modulepath}"
-puts "Moduledir:   #{moduledir}" 
+puts "Moduledir:   #{moduledir}"
 
 info "set symlink to the '#{basedir}' module for local developments"
 run %{ ln -s #{basedir} #{moduledir}/#{name}  } unless File.exists?("#{moduledir}/#{name}")
 
 # Prepare hiera
-unless File.exists?('/etc/puppet/hiera.yaml')
-    run %{ ln -s /etc/hiera.yaml /etc/puppet/hiera.yaml } if File.exists?("/etc/hiera.yaml")
-end
-# # hieracfg = YAML::load_file('/etc/hiera.yaml')
-# # [ '/vagrant/tests/hiera' ].each do |d|
-# #     hieracfg[:datadir] = [] if hieracfg[:datadir].nil?
-# #     hieracfg[:datadir] << d #if File.directory?('#{d}')
-# # end
-# # hieracfg[:hierarchy] = [] if hieracfg[:hierarchy].nil?
-# # hieracfg[:hierarchy] << 'common' unless hieracfg[:hierarchy].include?('common')
-hieracfg = {
-    :backends  => [ 'yaml' ],
-    :hierarchy => [ 'defaults', 'common' ],
-    :yaml      => {
-        :datadir   => '/vagrant/tests/hiera',
-    }
-}
-FalkorLib::Common.store_config('/etc/hiera.yaml', hieracfg, {:no_interaction => true})
+# unless File.exists?('/etc/puppet/hiera.yaml')
+#     run %{ ln -s /etc/hiera.yaml /etc/puppet/hiera.yaml } if File.exists?("/etc/hiera.yaml")
+# end
+# # # hieracfg = YAML::load_file('/etc/hiera.yaml')
+# # # [ '/vagrant/tests/hiera' ].each do |d|
+# # #     hieracfg[:datadir] = [] if hieracfg[:datadir].nil?
+# # #     hieracfg[:datadir] << d #if File.directory?('#{d}')
+# # # end
+# # # hieracfg[:hierarchy] = [] if hieracfg[:hierarchy].nil?
+# # # hieracfg[:hierarchy] << 'common' unless hieracfg[:hierarchy].include?('common')
+# hieracfg = {
+#     :backends  => [ 'yaml' ],
+#     :hierarchy => [ 'defaults', 'common' ],
+#     :yaml      => {
+#         :datadir   => '/vagrant/tests/hiera',
+#     }
+# }
+# FalkorLib::Common.store_config('/etc/hiera.yaml', hieracfg, {:no_interaction => true})
