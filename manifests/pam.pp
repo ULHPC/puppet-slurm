d62c83fcc5f4d395d451f693b3b74417c473a18b
Sebastien Varrette <Sebastien.Varrette@uni.lu>
2017-08-21 14:07:18 +0200

    slurm::pam
    Signed-off-by: Sebastien Varrette <Sebastien.Varrette@uni.lu>

--- manifests/pam.pp
+++ manifests/pam.pp
@@ -1,5 +1,5 @@
 ################################################################################
-# Time-stamp: <Sun 2017-08-20 17:51 svarrette>
+# Time-stamp: <Mon 2017-08-21 14:04 svarrette>
 #
 # File::      <tt>pam.pp</tt>
 # Author::    UL HPC Team (hpc-sysadmins@uni.lu)
@@ -9,25 +9,42 @@
 # ------------------------------------------------------------------------------
 # = Class: slurm::pam
 #
-# Setup Pluggable Authentication Modules (PAM) (i.e. the module 'pam_slurm') for slurmd
+# Setup Pluggable Authentication Modules (PAM) (i.e. the module 'pam_slurm') for slurmd,
+# i.e. /etc/pam.d/slurm and
 # See https://slurm.schedmd.com/faq.html#pam
+#
+# == Parameters
+#
+# @param ensure  [String]  Default: 'present'
+#        Ensure the presence (or absence) of sysctl
+# @param content [String] Default: see 'templates/pam_slurm.erb'
+#
+#
+#
+# /!\ We assume the RPM 'slurm-pam_slurm' has been already installed
 
 class slurm::pam(
-  $ensure  = $slurm::ensure,
-  $content = template('slurm/pam_slurm.erb'),
-  $lines   = undef,
+  String $ensure  = $slurm::params::ensure,
+  String $content = template('slurm/pam_slurm.erb'),
+  String $limits_source = "puppet:///modules/slurm/limits.memlock"
 )
+inherits slurm::params
 {
-  validate_re($ensure, ['^present$', '^absent$'] )
-
-  # We assume the RPM 'slurm-pam_slurm' has been already installed
+  validate_legacy('String', 'validate_re', $ensure, ['^present', '^absent'])
 
   # TODO: Enable SLURM's use of PAM by setting UsePAM=1 in slurm.conf.
+
   # Establish a PAM configuration file for slurm
   pam::service { "${slurm::params::pam_servicename}":
-    ensure  => "ensure",
-    content => $content,
-    line    => $lines
+    ensure  => $ensure,
+    content => $content
+  }
+  include pam::limits
+
+  # Update PAM MEMLOCK limits (required for MPI)
+  pam::limits::fragment { "${slurm::params::pam_servicename}":
+    ensure => $ensure,
+    source => $limits_source
   }
 
 }
