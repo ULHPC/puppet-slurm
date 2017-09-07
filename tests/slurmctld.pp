09b2d5b62aea2887bab1c995fad83272669195d5
Sebastien Varrette <Sebastien.Varrette@uni.lu>
2017-09-07 10:25:23 +0200

    test slurmctld class without service management
    Signed-off-by: Sebastien Varrette <Sebastien.Varrette@uni.lu>

---
+++ tests/slurmctld.pp
@@ -0,0 +1,13 @@
+#
+# You can execute this manifest as follows in your vagrant box
+#
+#      sudo puppet apply -t /vagrant/tests/slurmctld.pp
+#
+node default {
+  #include ::slurm::params
+
+  class { '::slurm':
+    service_manage => false,
+  }
+  include ::slurm::slurmctld
+}
