1cad4f501771406112a31995312a690fd965bdb3
Sebastien Varrette <Sebastien.Varrette@uni.lu>
2020-10-22 09:09:23 +0200

    [bugfix] slurmd string port range
    Signed-off-by: Sebastien Varrette <Sebastien.Varrette@uni.lu>

--- manifests/slurmd.pp
+++ manifests/slurmd.pp
@@ -30,10 +30,10 @@ class slurm::slurmd inherits slurm
   Class['slurm::install'] -> Class['slurm::config']
 
   if $slurm::manage_firewall {
-    slurm::firewall { $slurm::slurmdport:
+    slurm::firewall { "${slurm::slurmdport}":
       ensure => $slurm::ensure,
     }
-    slurm::firewall { $slurm::srunportrange:
+    slurm::firewall { "${slurm::srunportrange}":
       ensure => $slurm::ensure,
     }
   }
